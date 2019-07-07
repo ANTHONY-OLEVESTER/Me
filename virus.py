b6ada6de2e5eca81d64494d47a4b5ed230606df5
ANTHONY-OLEVESTER <47980189+ANTHONY-OLEVESTER@users.noreply.github.com>
2019-07-07 13:53:46 +0530

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,11 @@
+import numpy
+import Txt
+exit(temp.py)
+a[][]=read(readme.txt)
+code[][]=read(virus.py)
+b[][]=SaveAs(a[][]+code[][],readme.py)
+c[][]={{close(virus.py)},
+        {run(readme.py)}}
+SaveAs(c[][],temp.py)
+delete(virus.py)
+run(temp.py)
